224dbc80da258caa8aca694e30121883f162023b
abdulsagheer <abdulsagheeras29@gmail.com>
2023-01-14 13:25:33 +0530

    Added CRUD

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,71 +1,107 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-const Table = () => {
+import { getUsers } from "../lib/helper";
+import { useQuery } from "react-query";
+import { useSelector, useDispatch } from "react-redux";
+import {
+	toggleChangeAction,
+	updateAction,
+	deleteAction,
+} from "../redux/reducer";
+
+export default function Table() {
+	const { isLoading, isError, data, error } = useQuery("users", getUsers);
+
+	if (isLoading) return <div>Employee is Loading...</div>;
+	if (isError) return <div>Got Error {error}</div>;
+
 	return (
 		<table className="min-w-full table-auto">
 			<thead>
-				<tr>
-					<th className="px-15">
+				<tr className="bg-gray-800">
+					<th className="px-16 py-2">
 						<span className="text-gray-200">Name</span>
 					</th>
-					<th className="px-15">
+					<th className="px-16 py-2">
 						<span className="text-gray-200">Email</span>
 					</th>
-					<th className="px-15">
+					<th className="px-16 py-2">
 						<span className="text-gray-200">Salary</span>
 					</th>
-					<th className="px-15">
+					<th className="px-16 py-2">
 						<span className="text-gray-200">Birthday</span>
 					</th>
-					<th className="px-15">
+					<th className="px-16 py-2">
 						<span className="text-gray-200">Status</span>
 					</th>
-					<th className="px-15">
+					<th className="px-16 py-2">
 						<span className="text-gray-200">Actions</span>
 					</th>
 				</tr>
 			</thead>
 			<tbody className="bg-gray-200">
-				<tr className="bg-gray-50 text-center">
-					<td className="px-16 py-12 flex flex-row items-center">
-						<img
-							src="#"
-							alt="m"
-							className="h-8 w-8 rounded-full object-cover"
-						/>
-						<span className="text-center ml-2 font-semibold">
-							Daily Tuition
-						</span>
-					</td>
-					<td className="px-16 py-12">
-						<span>abdulsagheeras29@gmail.com</span>
-					</td>
-					<td className="px-16 py-12">
-						<span>60,000</span>
-					</td>
-					<td className="px-16 py-12">
-						<span>29/01/2001</span>
-					</td>
-					<td className="px-16 py-2">
-						<button className="cursor">
-							<span className="bg-green-500 text-white px-5 py-1 rounded-full">
-								Active
-							</span>
-						</button>
-					</td>
-					<td className="px-16 py-2 flex justify-around gap-5">
-						<button className="cursor">
-							<BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit>
-						</button>
-						<button className="cursor">
-							<BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt>
-						</button>
-					</td>
-				</tr>
+				{data && data?.map((obj, i) => <Tr {...obj} key={i} />)}
 			</tbody>
 		</table>
 	);
-};
+}
 
-export default Table;
+function Tr({ _id, name, avatar, email, salary, date, status }) {
+	const visible = useSelector((state) => state.app.client.toggleForm);
+	const dispatch = useDispatch();
+
+	const onUpdate = () => {
+		dispatch(toggleChangeAction(_id));
+		if (visible) {
+			dispatch(updateAction(_id));
+		}
+	};
+
+	const onDelete = () => {
+		if (!visible) {
+			dispatch(deleteAction(_id));
+		}
+	};
+
+	return (
+		<tr className="bg-gray-50 text-center">
+			<td className="px-16 py-2 flex flex-row items-center">
+				<img
+					src={avatar || "#"}
+					alt=""
+					className="h-8 w-8 rounded-full object-cover"
+				/>
+				<span className="text-center ml-2 font-semibold">
+					{name || "Unknown"}
+				</span>
+			</td>
+			<td className="px-16 py-2">
+				<span>{email || "Unknown"}</span>
+			</td>
+			<td className="px-16 py-2">
+				<span>{salary || "Unknown"}</span>
+			</td>
+			<td className="px-16 py-2">
+				<span>{date || "Unknown"}</span>
+			</td>
+			<td className="px-16 py-2">
+				<button className="cursor">
+					<span
+						className={`${
+							status == "Active" ? "bg-green-500" : "bg-rose-500"
+						} text-white px-5 py-1 rounded-full`}>
+						{status || "Unknown"}
+					</span>
+				</button>
+			</td>
+			<td className="px-16 py-2 flex justify-around gap-5">
+				<button className="cursor" onClick={onUpdate}>
+					<BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit>
+				</button>
+				<button className="cursor" onClick={onDelete}>
+					<BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt>
+				</button>
+			</td>
+		</tr>
+	);
+}
